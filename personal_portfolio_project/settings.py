38c3a70b9091a4aef6c19d5024d276f877cbd104
Mike Nedviga <crocodilchi@yandex.ru>
2022-07-17 11:12:09 +0000

    Remove almost all staff

--- personal_portfolio_project/settings.py
+++ personal_portfolio_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-58xr7om*&4(v-c2=!_%-vpxc320^4sa%6kseh*8&e0qm_y_f&n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['madcheddar.pythonanywhere.com']
 
 
 # Application definition
@@ -123,6 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
